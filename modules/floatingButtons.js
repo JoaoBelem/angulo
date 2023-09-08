0cce85ccf83629f5067d8bc385ea12f0369e59c2
Johnny <jpbelem45@outlook.com>
2023-09-08 01:24:31 -0300

    refatoração de algumas coisas

--- modules/floatingButtons.js
+++ modules/floatingButtons.js
@@ -3,12 +3,20 @@ const themeBtn = document.getElementById('themeBtn');
 export default function doFloatingButtons() {
   let theme = localStorage.theme === 'true';
 
+  theme
+    ? (themeBtn.innerText = 'Tema claro 🌞')
+    : (themeBtn.innerText = 'Tema escuro 🌙');
+
   if (theme) document.body.classList.add('dark');
 
   themeBtn.addEventListener('click', () => {
     theme = !theme;
 
     localStorage.theme = theme;
+
+    !!theme
+      ? (themeBtn.innerText = 'Tema claro 🌞')
+      : (themeBtn.innerText = 'Tema escuro 🌚');
     document.body.classList.toggle('dark');
   });
 }
